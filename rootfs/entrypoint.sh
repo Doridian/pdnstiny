0bc4e58721661b2f20bb46a79a98df930c587dfb
Doridian <git@doridian.net>
2025-09-22 11:53:58 -0700

    Only run services we want

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 set -e
 
-exec s6-svscan /etc/s6
+rm -rf /var/run/s6-services
+mkdir -p /var/run/s6-services
+
+found_service=0
+for svc in /etc/s6/*; do
+    if [ -f "$svc/should_run" ]; then
+        if ! "$svc/should_run"; then
+            echo "SKIPPING service $svc"
+            continue
+        fi
+    fi
+    echo "ENABLING service $svc"
+    ln -s "$svc" /var/run/s6-services/
+    found_service=1
+done
+
+if [ $found_service -eq 0 ]; then
+    echo 'No services found to enable. Exiting.'
+    exit 1
+fi
+
+exec s6-svscan /var/run/s6-services
